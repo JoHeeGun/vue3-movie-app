9067d5674ca136ed09a75c22b61c21fc7028b92a
JOHEEGUN <whgmlrjs123@naver.com>
2021-08-14 17:21:05 +0900

    init

---
+++ src/components/Logo.vue
@@ -0,0 +1,20 @@
+<template>
+  <RouterLink
+    to="/"
+    class="logo">
+    <span>Heegun's</span>OMDbAPI Site
+  </RouterLink>
+</template>
+
+<style lang="scss" scoped>
+
+.logo {
+  font-family: "Oswald", sans-serif;
+  font-size: 20px;
+  color: $black;
+  text-decoration: none;
+  span {
+    color:$primary
+  }
+}
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'JoHeeGun',
+    email: 'whgmlrjs123@naver.com',
+    blog: 'https://www.naver.com/',
+    phone: '+82-010-3940-6688',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}

165e8024fdabda6aee5f4dfa49ff441b7f54012f
JoHeeGun <whgmlrjs123@naver.com>
2022-02-05 16:08:52 +0900

    v3

---
+++ src/components/test2.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div>version3</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Vue3MovieTest',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

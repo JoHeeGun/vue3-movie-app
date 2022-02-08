b3ee6eecd729625d79bbbac59303117486402e57
JoHeeGun <whgmlrjs123@naver.com>
2022-02-08 21:56:59 +0900

    test2

---
+++ src/components/test22.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div>브랜치 test</div>
+        <div>브렌치 test22222test!!!</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Vue3MovieTest',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

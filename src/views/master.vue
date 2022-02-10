b8fc94668aad71fd89cae54fc292ab871e0f752f
DESKTOP-ATV123J\INSoft <whgmlrjs123@naver.com>
2022-02-10 15:00:10 +0900

    test

---
+++ src/views/master.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div>브랜치 test</div>
+        <div>브렌치 2-1 test!!!</div>\
+        <div>asd</div>
+        <div>ss</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Vue3MovieTest',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

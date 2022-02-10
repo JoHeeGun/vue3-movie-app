88bb2803094f749bda5fc04412b6ad1624ca9df8
heegun <heegun@in-soft.co.kr>
2022-02-10 09:31:44 +0900

    d

---
+++ src/views/qq.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div>브랜치 test</div>
+        <div>브렌치 2-1 test!!!</div>\
+        <div>asd</div>
+        <div>asd222</div>
+        <div>qqqq2</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Vue3MovieTest',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
